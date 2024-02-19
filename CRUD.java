5982f599b6ca2dff4e3ba8859bc2caa197425d6f
Vadim <vadimgrachev416@gmail.com>
2024-02-18 23:54:09 -0500

    added PostgreSQL interaction

--- CRUD.java
+++ CRUD.java
@@ -7,54 +7,43 @@ import java.sql.*;
 
 public class CRUD {
     static boolean running = true;
-    static menu menu;
-    static String[][] mealDB = new String[10][4];
-    static int mealsInDB = 0;
     static BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
-    String DB_URL = "jdbc:postgresql:meals_db";
-    String USER = "postgres";
-    String PASS = "1111";
+    static String category = "";
+    static String meal = "";
+    static String ingredients = "";
+    static int meal_id = 0;
 
-    Connection connection;
+    public static void CRUDInterface(String input, Connection connection, Statement statement) throws IOException, SQLException {
 
-    {
-        try {
-            connection = DriverManager.getConnection(DB_URL, USER, PASS);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    //connection.setAutoCommit(true);
-
-    public static void CRUDInterface(String input) throws IOException {
-        if (menu == menu.actionMenu) {
             switch (input) {
-                case "add" -> add(input);
-                case "show" -> show();
+                case "add" -> {
+                    add(input);
+                    statement.executeUpdate("insert into meals (category, meal, meal_id) values ('" + category + "', '" + meal + "', " + meal_id + ")");
+                    statement.executeUpdate("insert into ingredients (ingredient, ingredient_id, meal_id) values ('" + ingredients + "', '" + meal_id + "', " + meal_id + ")");
+                }
+                case "show" -> show(connection);
                 case "exit" -> {
                     running = false;
                     System.out.println("Bye!");
                 }
-                //default -> running = false;
                 default -> throw new IllegalStateException("Unexpected value: " + input);
             }
-        } else if (menu == menu.CRUDMenu) {
-        }
+
     }
-    public static void add(String input) throws IOException {
+    public static void add(String input) throws IOException, SQLException {
         System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
             while(true) {
                 input = buf.readLine();
                 if (input.matches("breakfast|lunch|dinner") && !input.equals(" ")) {
-                    mealDB[mealsInDB][1] = input;
+                    category = input;
                     break;
                 } else System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
             }
             while(true) {
                 System.out.println("Input the meal's name:");
                 input = buf.readLine();
-                if (input.matches("^[a-zA-Z]+[\s]*[a-zA-Z]*$") && !input.equals("")) {
-                    mealDB[mealsInDB][2] = input;
+                if (input.matches("^[a-zA-Z]+[\s]*[a-zA-Z]*$") && !input.equals(" ")) {
+                    meal = input;
                     break;
                 } else System.out.println("Wrong format. Use letters only!");
             }
@@ -63,24 +52,32 @@ public class CRUD {
                 input = buf.readLine();
                 if (input.matches("[a-zA-Z,\s]+") &&
                         !input.matches(".+,\s*,.+|.+,\s*")) {
-                    mealDB[mealsInDB][3] = input;
+                    ingredients = input;
                     break;
                 } else System.out.println("Wrong format. Use letters only!");
             }
-            mealsInDB++;
+            meal_id++;
             System.out.println("The meal has been added!");
-            menu = menu.actionMenu;
             }
 
-    public static void show() {
-        if (mealsInDB > 0) {
-            for (int i = 0; i < mealsInDB ; i++) {
-                System.out.printf("\nCategory: %s\nName: %s\nIngredients:\n", mealDB[i][1], mealDB[i][2]);
-                String[] ingredients = mealDB[i][3].split(",\s*");
-                for (int j = 0; j < ingredients.length; j++) {
-                    System.out.println(ingredients[j]);
+    public static void show(Connection connection) throws SQLException {
+
+            PreparedStatement psMeal = connection.prepareStatement("SELECT * FROM meals");
+            ResultSet rsMeal = psMeal.executeQuery();
+            while (rsMeal.next()) {
+                System.out.println("Category: " + rsMeal.getString("category"));
+                System.out.println("Name: " + rsMeal.getString("meal"));
+                int meal_id = rsMeal.getInt("meal_id");
+                PreparedStatement psIngredient = connection.prepareStatement("SELECT * FROM ingredients WHERE meal_id = " + meal_id);
+                ResultSet rsIngredient = psIngredient.executeQuery();
+                System.out.println("Ingredients:");
+                while (rsIngredient.next()) {
+                    String[] ingredients = rsIngredient.getString("ingredient").split(",\s*");
+                    for (String ingredient : ingredients) {
+                        System.out.println(ingredient);
+                    }
                 }
+                System.out.println();
             }
-        } else System.out.println("No meals saved. Add a meal first.");
     }
 }
--- Main.java
+++ Main.java
@@ -1,24 +1,44 @@
 package mealplanner;
 
 import java.io.IOException;
+import java.sql.*;
 
 import static mealplanner.CRUD.*;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, SQLException {
 
-    menu = menu.actionMenu;
+    String DB_URL = "jdbc:postgresql:meals_db";
+    String USER = "postgres";
+    String PASS = "1111";
+    Connection connection;
+    {
+      try {
+        connection = DriverManager.getConnection(DB_URL, USER, PASS);
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    connection.setAutoCommit(true);
+
+    Statement statement = connection.createStatement();
+    //statement.executeUpdate("drop table if exists meals");
+    //statement.executeUpdate("drop table if exists ingredients");
+    statement.executeUpdate("CREATE TABLE IF NOT EXISTS meals(" +
+            "category VARCHAR(10) NOT NULL," +
+            "meal VARCHAR(20) NOT NULL," +
+            "meal_id INTEGER PRIMARY KEY)");
+    statement.executeUpdate("CREATE TABLE IF NOT EXISTS ingredients(" +
+            "ingredient VARCHAR(255) NOT NULL," +
+            "ingredient_id INTEGER NOT NULL," +
+            "meal_id INTEGER NOT NULL)");
+
     while (running) {
-      if (menu == menu.actionMenu) {
         System.out.println("What would you like to do (add, show, exit)?");
-      }
-      if (menu == menu.CRUDMenu) {
-        System.out.println("");
-      }
       while(true) {
         String input = buf.readLine();
         if (input.matches("add|show|exit") && !input.equals(" ")) {
-          CRUDInterface(input);
+          CRUDInterface(input, connection, statement);
           break;
         } else break;
       }
